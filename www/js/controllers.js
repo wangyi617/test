5a3540bd8653a1f5de3b6cfdb997fc2c15598eb3
sorashiro <396800605@qq.com>
2016-02-02 15:22:10 +0800

    update

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,32 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $http, $ionicPopup) {
+  $scope.checkUpdate =  function() {
+    // cordova.getAppVersion.getVersionNumber(function (versionNumber) {
+      var url="https://api.github.com/repos/wangyi617/test/releases/latest";
+      $http.get(url).success(function (data) {
+        // if (versionNumber < data.versionNumber) {
+        //   var confirmPopup = $ionicPopup.confirm({
+        //     title: "有新版本升级了",
+        //     template: data.msg,
+        //     cancelText: "取消",
+        //     okText: "升级"
+        //   });
+        //   confirmPopup.then(function (res) {
+        //     if (res) {
+        //       window.open(data.apk, '_system', 'location=yes');
+        //     } else {
+        //     }
+        //   })
+        // }
+        consolo.log(data);
+      })
+      // .error(function (error) {
+      //     Toast.show("对不起更新失败")
+      // })
+    // });
+  };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
